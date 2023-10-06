cc56c8e391078dc5589b07a6a28be5d704d7f630
Aaron2963 <neotgrr@gmail.com>
2023-10-06 23:33:40 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,6 +1,52 @@
 # Fish Sushi Order Downloader
 
+Current Version: v1.0.0
+
 ## Description
 
 This is a simple script to download the orders from the Fish Sushi website.
+
+## Usage
+
+After cloning the repository, run the following commands to install the dependencies:
+
+```bash
+npm install
+```
+
+To run the script:
+
+```bash
+npm run start
+```
+
+To build binaries for Windows 64-bit, run the following command:
+
+```bash
+npm run build
+```
+
+To change the source remote path and local directory path, edit the `config.json` file, and build the project again:
+
+```json
+{
+  "host": "35.194.203.36",
+  "port": 22,
+  "username": "rsa-key-20200213",
+  "privateKeyPath": "C:\\Users\\Administrator\\Desktop\\WinSCP Script\\rsa-key-20200213.ppk",
+  "targets": [
+    {
+      "from": "/home/rsa-key-20200213/tastefresh/argo-sal",  // source remote path #1
+      "to": "C:\\Users\\Administrator\\Documents\\網站ERP銷貨單資料夾勿動"  // local directory path #1
+    },
+    {
+      "from": "/home/rsa-key-20200213/tastefresh/argo-odr",  // source remote path #2
+      "to": "C:\\Users\\Administrator\\Documents\\網站ERP訂單資料夾勿動"  // local directory path #2
+    }
+  ]
+}
+```
+
+
+
 
--- index.js
+++ index.js
@@ -1,24 +1,81 @@
 const Client = require('ssh2-sftp-client')
 const fs = require('fs')
+const path = require('path')
 const config = require('./config.json')
 const sftp = new Client()
+const logPath = path.join(__dirname, 'log', 'downloader.log')
+
+class Logger {
+  logLimit = 1000
+  constructor(logPath) {
+    this.path = logPath
+    this.lineNum = 0
+    try {
+      // check if log file exists
+      if (!fs.existsSync(path.dirname(logPath))) {
+        fs.mkdirSync(path.dirname(logPath), { recursive: true })
+      }
+      if (!fs.existsSync(logPath)) {
+        fs.writeFileSync(logPath, '')
+      }
+      // count lines
+      const log = fs.readFileSync(logPath, 'utf8')
+      for (let i = 0; i < log.length; i++) {
+        if (log[i] === '\n') {
+          this.lineNum++
+        }
+      }
+    } catch (error) {
+      console.log(`failed to create log file: ${error}`)
+      process.exit(1)
+    }
+  }
+
+  append(message) {
+    if (this.lineNum >= this.logLimit) {
+      this.switchFile()
+      this.lineNum = 0
+    }
+    const d = new Date()
+    const time = d.toISOString().substring(0, 19)
+    const log = `[${time}] ${message}\n`
+    console.log(message)
+    fs.appendFileSync(this.path, log)
+    this.lineNum++
+  }
+
+  switchFile() {
+    const dir = path.dirname(this.path)
+    const filename = path.basename(this.path)
+    const d = new Date().toISOString().substring(0, 16).replace(/[-:]/g, '')
+    const newFilename = `${d}_${filename}`
+    fs.renameSync(this.path, path.join(dir, newFilename))
+    fs.writeFileSync(this.path, '')
+  }
+}
 
 class Connection {
-  constructor(sftpClient, config) {
+  constructor(sftpClient, config, logger) {
     this.config = config
     this.client = sftpClient
+    this.logger = logger
     const d = new Date()
-    this.time = `${d.getHours().toString().padStart(2, '0')}${d.getMinutes().toString().padStart(2, '0')}` + 
+    this.time = `${d.getHours().toString().padStart(2, '0')}${d.getMinutes().toString().padStart(2, '0')}` +
       `${d.getSeconds().toString().padStart(2, '0')}${d.getMilliseconds().toString().padStart(3, '0')}`
   }
 
   async connect() {
-    await this.client.connect({
-      host: this.config.host,
-      port: this.config.port,
-      username: this.config.username,
-      privateKey: fs.readFileSync(this.config.privateKeyPath)
-    })
+    try {
+      await this.client.connect({
+        host: this.config.host,
+        port: this.config.port,
+        username: this.config.username,
+        privateKey: fs.readFileSync(this.config.privateKeyPath)
+      })
+    } catch (error) {
+      this.logger.append(`failed to connect: ${error}`)
+      process.exit(1)
+    }
     return
   }
 
@@ -41,8 +98,12 @@ class Connection {
       const remote = filenames[i],
         filename = remote.split('/').pop(),
         local = `${context}/${this.getTimedName(filename)}`
-      console.log(`downloading ${local} from ${remote}...`)
-      await this.client.fastGet(remote, local)
+      try {
+        await this.client.fastGet(remote, local)
+        this.logger.append(`download ${local} from ${remote}`)
+      } catch (error) {
+        this.logger.append(`failed to download ${remote}: ${error}`)
+      }
     }
     return;
   }
@@ -57,10 +118,10 @@ class Connection {
     const hasDir = await this.client.exists(backupDir)
     if (!hasDir) {
       try {
-        console.log(`creating ${backupDir}...`)
         await this.client.mkdir(backupDir)
+        this.logger.append(`create ${backupDir}`)
       } catch (error) {
-        console.log(error)
+        this.logger.append(`failed to create ${backupDir}: ${error}`)
       }
     }
     // move files to backup dir
@@ -69,29 +130,29 @@ class Connection {
       const remote = `${remotePath}/${filename}`,
         backup = `${backupDir}/${this.getTimedName(filename)}`
       try {
-        console.log(`moving ${remote} to ${backup}...`)
         await this.client.rename(remote, backup)
+        this.logger.append(`moved ${remote} to ${backup}`)
       } catch (err) {
-        console.log(err)
+        this.logger.append(`failed to move ${remote} to ${backup}: ${err}`)
       }
     }
   }
-
-
 }
 
 async function main() {
-  console.log(`connecting to ${config.username}@${config.host}:${config.port}`)
-  const conn = new Connection(sftp, config)
+  const logger = new Logger(logPath)
+  logger.append(`connecting to ${config.username}@${config.host}:${config.port}`)
+  const conn = new Connection(sftp, config, logger)
   await conn.connect()
   for (let i = 0; i < config.targets.length; i++) {
     const target = config.targets[i]
     const files = await conn.listFilenames(target.from)
-    console.log(`found ${files.length} files from ${target.from}`)
+    logger.append(`found ${files.length} files from ${target.from}`)
     await conn.download(target.to, files)
     await conn.moveToOld(files)
   }
   conn.end()
+  process.exit(0)
 }
 
 main()
